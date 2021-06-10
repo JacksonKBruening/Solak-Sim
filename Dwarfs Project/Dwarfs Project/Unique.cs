52f56188e7be65e63f6bbe6624e6032475b17a13
ThatOneEngineer25 <85266719+ThatOneEngineer25@users.noreply.github.com>
2021-06-10 17:04:02 -0500

    Add files via upload
    A new class that handles a variable for the unique drops!

---
+++ Dwarfs Project/Dwarfs Project/Unique.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dwarfs_Project
+{
+    public class Unique : Drop
+    {
+        public int Personal { get; set; } //Stores the number of personals
+
+        public Unique(string name, int valMod) : base(name, valMod)
+        {
+            Personal = 0;
+        }
+
+    }
+}
